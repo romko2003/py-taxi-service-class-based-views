025568cd45f7e22b35681bd4b4e1a91e05acfe22
= <=>
2025-07-14 00:04:16 +0300

    implement

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
-def index(request):
-    """View function for the home page of the site."""
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
 
-    context = {
-        "num_drivers": Driver.objects.count(),
-        "num_cars": Car.objects.count(),
-        "num_manufacturers": Manufacturer.objects.count(),
-    }
 
-    return render(request, "taxi/index.html", context=context)
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer").prefetch_related("drivers")
+    paginate_by = 5
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related("car_set__manufacturer")
