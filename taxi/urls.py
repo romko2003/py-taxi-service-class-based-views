483d5679b9c2e833a9144e988da90c5d69458894
= <=>
2025-07-14 00:11:03 +0300

    implement

--- taxi/urls.py
+++ taxi/urls.py
@@ -7,12 +7,17 @@ from .views import (
     DriverDetailView,
 )
 
+app_name = "taxi"
+
 urlpatterns = [
     path("manufacturers/", ManufacturerListView.as_view(),
          name="manufacturer-list"),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("cars/", CarListView.as_view(),
+         name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(),
+         name="car-detail"),
+    path("drivers/", DriverListView.as_view(),
+         name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(),
          name="driver-detail"),
 ]
