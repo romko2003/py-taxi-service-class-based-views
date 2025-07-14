4fdac90316bb1171d0968b7f693d3e49fbadc51a
= <=>
2025-07-14 17:07:04 +0300

    implement

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,20 +1,27 @@
 from django.urls import path
+
 from .views import (
+    index,
     ManufacturerListView,
     CarListView,
     CarDetailView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverUpdateView,
+    DriverDeleteView,
 )
 
-app_name = "taxi"
-
 urlpatterns = [
-    path("manufacturers/", ManufacturerListView.as_view(),
-         name="manufacturer-list"),
-    path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>/", DriverDetailView.as_view(),
-         name="driver-detail"),
+    path('', index, name='index'),
+    path('manufacturers/', ManufacturerListView.as_view(), name='manufacturer-list'),
+    path('cars/', CarListView.as_view(), name='car-list'),
+    path('cars/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
+    path('drivers/', DriverListView.as_view(), name='driver-list'),
+    path('drivers/<int:pk>/', DriverDetailView.as_view(), name='driver-detail'),
+    path('drivers/create/', DriverCreateView.as_view(), name='driver-create'),
+    path('drivers/<int:pk>/update/', DriverUpdateView.as_view(), name='driver-update'),
+    path('drivers/<int:pk>/delete/', DriverDeleteView.as_view(), name='driver-delete'),
 ]
+
+app_name = 'taxi'
--- taxi/views.py
+++ taxi/views.py
@@ -1,32 +1,71 @@
-from django.views.generic import ListView, DetailView
-from .models import Manufacturer, Car, Driver
-from django.db.models import Prefetch
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from .models import Driver, Car, Manufacturer
 
 
-class ManufacturerListView(ListView):
+def index(request):
+    """View function for the home page of the site."""
+    num_drivers = Driver.objects.count()
+    num_cars = Car.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_drivers': num_drivers,
+        'num_cars': num_cars,
+        'num_manufacturers': num_manufacturers,
+        'num_visits': num_visits + 1,
+    }
+
+    return render(request, 'taxi/index.html', context=context)
+
+
+class ManufacturerListView(generic.ListView):
     model = Manufacturer
-    queryset = Manufacturer.objects.all().order_by("name")
+    queryset = Manufacturer.objects.all().order_by('name')
     paginate_by = 5
 
 
-class CarListView(ListView):
+class CarListView(generic.ListView):
     model = Car
-    queryset = (Car.objects.
-                select_related("manufacturer").prefetch_related("drivers"))
     paginate_by = 5
+    queryset = Car.objects.select_related('manufacturer')
 
 
-class CarDetailView(DetailView):
+class CarDetailView(generic.DetailView):
     model = Car
 
 
-class DriverListView(ListView):
+class DriverListView(generic.ListView):
     model = Driver
     paginate_by = 5
 
 
-class DriverDetailView(DetailView):
+class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related(
-        Prefetch("cars", queryset=Car.objects.select_related("manufacturer"))
-    )
+    queryset = Driver.objects.prefetch_related('cars__manufacturer')
+
+
+@method_decorator(login_required, name='dispatch')
+class DriverCreateView(generic.CreateView):
+    model = Driver
+    fields = '__all__'
+    success_url = '/drivers/'
+
+
+@method_decorator(login_required, name='dispatch')
+class DriverUpdateView(generic.UpdateView):
+    model = Driver
+    fields = '__all__'
+    success_url = '/drivers/'
+
+
+@method_decorator(login_required, name='dispatch')
+class DriverDeleteView(generic.DeleteView):
+    model = Driver
+    success_url = '/drivers/'
